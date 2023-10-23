d0fbba7999dd3c37c3000c3300feab52ee9cdfe4
averosmusic <143966870+averosmusic@users.noreply.github.com>
2023-10-22 22:36:12 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+// const openMenu = document.getElementById('openMenu');
+
+// openMenu.addEventListener('click', function () {
+//     document.querySelectorAll('.nav-desktop').forEach(function (element) {
+//         element.style.display = 'flex';
+//     });
+// });
+

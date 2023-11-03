2d9d23598a6c1380bde583eb9c9283120a667c83
averosmusic <143966870+averosmusic@users.noreply.github.com>
2023-11-03 00:16:43 -0400

    Botones de disponibles y proximos agregados

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-// const openMenu = document.getElementById('openMenu');
+function mostrarLista(tipo) {
+    // Oculta ambas listas
+    document.querySelector('.listaDisponibles').style.display = 'none';
+    document.querySelector('.listaProximos').style.display = 'none';
 
-// openMenu.addEventListener('click', function () {
-//     document.querySelectorAll('.nav-desktop').forEach(function (element) {
-//         element.style.display = 'flex';
-//     });
-// });
+    // Desactiva ambos botones
+    document.querySelector('.botones.disponibles').classList.remove('activo');
+    document.querySelector('.botones.proximos').classList.remove('activo');
 
+    // Muestra la lista y activa el botón correspondiente
+    if (tipo === 'disponibles') {
+        document.querySelector('.listaDisponibles').style.display = 'grid';
+        document.querySelector('.botones.disponibles').classList.add('activo');
+    } else if (tipo === 'proximos') {
+        document.querySelector('.listaProximos').style.display = 'grid';
+        document.querySelector('.botones.proximos').classList.add('activo');
+    }
+}

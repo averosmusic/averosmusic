85dec1e72e9226b1e0fafefc873a382c533ce2aa
averosmusic <143966870+averosmusic@users.noreply.github.com>
2023-11-03 00:30:33 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 function mostrarLista(tipo) {
-    // Oculta ambas listas
     document.querySelector('.listaDisponibles').style.display = 'none';
     document.querySelector('.listaProximos').style.display = 'none';
 
-    // Desactiva ambos botones
     document.querySelector('.botones.disponibles').classList.remove('activo');
     document.querySelector('.botones.proximos').classList.remove('activo');
 
-    // Muestra la lista y activa el botón correspondiente
     if (tipo === 'disponibles') {
         document.querySelector('.listaDisponibles').style.display = 'grid';
         document.querySelector('.botones.disponibles').classList.add('activo');

de47ddf120a7c14277d53df19509e14403d7b7f3
averosmusic <143966870+averosmusic@users.noreply.github.com>
2023-11-06 23:33:45 -0500

    segundo articulo agregado

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// funciones para los botones de "Ya disponibles" y "Proximos Lanzamientos"
 function mostrarLista(tipo) {
     document.querySelector('.listaDisponibles').style.display = 'none';
     document.querySelector('.listaProximos').style.display = 'none';
@@ -14,29 +15,36 @@ function mostrarLista(tipo) {
     }
 }
 
-function cambiarCarrousel() {
-    const prevButton = document.querySelector("#prev");
-    const nextButton = document.querySelector("#next");
-    const images = document.querySelectorAll('.carrousel img');
-    let currentIndex = 0;
 
-    images[currentIndex].classList.add('active');
-    images.forEach(image => {
-        image.classList.add('fade');
-    });
+
+const carrouseles = document.querySelectorAll('.carrousel')
+
+carrouseles.forEach(carrousel=>{
+    function cambiarCarrousel() {
+        const prevButton = carrousel.querySelector("#prev");
+        const nextButton = carrousel.querySelector("#next");
+        const images = carrousel.querySelectorAll('.carrousel img');
+        let currentIndex = 0;
     
-
-    nextButton.addEventListener('click', () => {
-        images[currentIndex].classList.remove('active');
-        currentIndex = (currentIndex + 1) % images.length;
         images[currentIndex].classList.add('active');
-    });
+        images.forEach(image => {
+            image.classList.add('fade');
+        });
+        
+    
+        nextButton.addEventListener('click', () => {
+            images[currentIndex].classList.remove('active');
+            currentIndex = (currentIndex + 1) % images.length;
+            images[currentIndex].classList.add('active');
+        });
+    
+        prevButton.addEventListener('click', () => {
+            images[currentIndex].classList.remove('active');
+            currentIndex = (currentIndex - 1 + images.length) % images.length;
+            images[currentIndex].classList.add('active');
+        });
+    }
+    
+    cambiarCarrousel();
+})
 
-    prevButton.addEventListener('click', () => {
-        images[currentIndex].classList.remove('active');
-        currentIndex = (currentIndex - 1 + images.length) % images.length;
-        images[currentIndex].classList.add('active');
-    });
-}
-
-cambiarCarrousel();
